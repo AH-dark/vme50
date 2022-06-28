365111305bb8b59e6ecd840de58cc36546a54e08
AH-dark <ahdark@outlook.com>
2022-06-28 14:08:16 +0800

    Fix: cors credential error

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/AH-dark/random-donate/controller"
+	"github.com/AH-dark/random-donate/pkg/conf"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,26 @@ func InitRouter() *gin.Engine {
 	r := gin.Default()
 
 	// cors
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			if gin.Mode() != gin.ReleaseMode {
+				return true
+			}
+
+			for _, v := range conf.CORSConfig.AllowOrigins {
+				if v == origin {
+					return true
+				}
+			}
+
+			return false
+		},
+		AllowMethods:     conf.CORSConfig.AllowMethods,
+		AllowHeaders:     conf.CORSConfig.AllowHeaders,
+		AllowCredentials: true,
+		ExposeHeaders:    conf.CORSConfig.ExposeHeaders,
+		MaxAge:           3600,
+	}))
 	// gzip
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
