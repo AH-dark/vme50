4b8d6cc2cda76e56a8e893f5807cb8e3f9eca113
AH-dark <ahdark@outlook.com>
2022-07-02 12:22:05 +0800

    Fix: go-sqlite3 need CGO

--- build.sh
+++ build.sh
@@ -1,15 +1,67 @@
 #!/usr/bin/bash
 
-export CGO_ENABLED=0
+REPO=$(
+  cd $(dirname $0) || exit
+  pwd
+)
 
-export GOOS=linux GOARCH=amd64
-go build -a -o build/linux_amd64_random_donate .
+ASSETS="false"
+BINARY="false"
 
-export GOOS=linux GOARCH=arm64
-go build -a -o build/linux_arm64_random_donate .
+buildAssets() {
+  cd $REPO || exit
+  rm -rf assets/out
 
-export GOOS=darwin GOARCH=amd64
-go build -a -o build/darwin_amd64_random_donate .
+  cd $REPO/assets || exit
+  yarn install --frozen-lockfile
+  yarn export
+}
 
-export GOOS=windows GOARCH=amd64
-go build -a -o build/windows_amd64_random_donate.exe .
+buildBinary() {
+  # linux amd64
+  export GOOS=linux GOARCH=amd64
+  go build -a -o build/linux_amd64_random_donate .
+
+  # linux arm64
+  export GOOS=linux GOARCH=arm64
+  go build -a -o build/linux_arm64_random_donate .
+
+  # darwin amd64
+  export GOOS=darwin GOARCH=amd64
+  go build -a -o build/darwin_amd64_random_donate .
+
+  # windows amd64
+  export GOOS=windows GOARCH=amd64
+  go build -a -o build/windows_amd64_random_donate.exe .
+}
+
+usage() {
+  echo "Usage: $0 [-a] [-b] [-f]" 1>&2
+  exit 1
+}
+
+while getopts "abf" o; do
+  case "${o}" in
+  a)
+    ASSETS="true"
+    BINARY="true"
+    ;;
+  f)
+    ASSETS="true"
+    ;;
+  b)
+    BINARY="true"
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+if [ "$ASSETS" = "true" ]; then
+  buildAssets
+fi
+
+if [ "$BINARY" = "true" ]; then
+  buildBinary
+fi
